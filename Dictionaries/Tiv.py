ae6a190ff3bd8240e1b9d603d1eec59944217f00
oluwatobilobaadeleke-debug <oluwatobiloba.adeleke@binghamuni.edu.ng>
2025-12-17 11:33:04 -0800

    Update Tiv.py

--- Dictionaries/Tiv.py
+++ Dictionaries/Tiv.py
@@ -1,1 +1,26 @@
+tiv_dictionary = {
+    "Water": "Mnger",
+    "Food": "Ior",
+    "House": "Akaa",
+    "Person": "Or",
+    "Child": "Wan",
+    "Man": "Ortyo",
+    "Woman": "Kwase",
+    "Sun": "Ter",
+    "Moon": "Ikyoo",
+    "Fire": "Mimi",
+    "Road": "Ityough",
+    "Tree": "Ikyer",
+    "Dog": "Mbwa",
+    "Goat": "Ishur",
+    "Cow": "Nom",
+    "Market": "Mba",
+    "River": "Mnger u shima",
+    "Friend": "Mngu",
+    "Night": "Yange",
+    "Day": "Aondo"
+}
+
+print(tiv_dictionary)
+
 
