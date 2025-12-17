948e9b314dae4d088e349e2a8161b22a646b1757
IdaraNnamso <excellencennamso@gmail.com>
2025-12-17 12:21:05 +0100

    Add Yoruba-English dictionary in Tiv.py

---
+++ Dictionaries/Tiv.py
@@ -0,0 +1,23 @@
+dictionary = {
+  
+    "Man": "Ọkùnrin",
+    "Woman": "Obìnrin",
+    "Child": "Ọmọ",
+    "House": "Ilé",
+    "Food": "Oúnjẹ",
+    "Water": "Omi",
+    "Sun": "Oòrùn",
+    "Moon": "Oṣùpá",
+    "Fire": "Iná",
+    "Work": "Iṣẹ́",
+    "Love": "Ìfẹ́",
+    "Friend": "Ọ̀rẹ́",
+    "Money": "Owó",
+    "School": "Ilé-ẹ̀kọ́",
+    "Book": "Ìwé",
+    "Road": "Ọ̀nà",
+    "Day": "Ọjọ́",
+    "Night": "Alẹ́",
+    "Time": "Àkókò",
+    "Life": "Ìyè"
+}
