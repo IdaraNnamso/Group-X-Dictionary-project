74f910cb5deb764f0714408f1ee52cf251483102
ogbonnarichard62-cmyk <ogbonnarichard62@gmail.com>
2025-12-17 13:51:17 +0100

    Update Igbo.py

---
+++ Dictionaries/Igbo.py
@@ -0,0 +1,23 @@
+dictionary = {
+  
+    "hello": "ndewo",
+    "goodbye": "ka o di",
+    "water": "mmiri",
+    "food": "nri",
+    "house": "ulo",
+    "man": "nwoke",
+    "woman": "nwanyi",
+    "child": "nwa",
+    "book": "akwukwo",
+    "school": "ulo akwukwo",
+    "love": "ihunanya",
+    "money": "ego",
+    "sun": "anyanwu",
+    "moon": "onwa",
+    "road": "uzo",
+    "friend": "enyi",
+    "market": "ahia",
+    "teacher": "onye nkuzi",
+    "student": "akwukwo",
+    "car": "ugbo ala"
+}
